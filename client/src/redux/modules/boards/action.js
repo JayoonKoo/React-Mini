473f46c8a1021faf766c2546a2c3615b109c6ec3
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-19 18:30:34 +0900

    feat: boards 가져오기 redux 구현

--- client/src/redux/modules/boards/action.js
+++ client/src/redux/modules/boards/action.js
@@ -1,7 +1,10 @@
-import {createActions} from 'redux-actions';
+import {createAction, createActions} from 'redux-actions';
 
 const prefixBoards = 'client/boards';
 const prefixBoard = 'client/board';
+export const START_UP = 'client/start/START_UP';
+export const CLEAN_START = 'client/start/CLEAN_START';
+export const SET_LENGTH = 'client/start/SET_LENGTH';
 
 export const boards = createActions('PENDING', 'SUCCESS', 'FAIL', {
   prefix: prefixBoards,
@@ -10,3 +13,7 @@ export const boards = createActions('PENDING', 'SUCCESS', 'FAIL', {
 export const board = createActions('PENDING', 'SUCCESS', 'FAIL', {
   prefix: prefixBoard,
 });
+
+export const startUp = createAction(START_UP);
+export const cleanStart = createAction(CLEAN_START);
+export const setLength = createAction(SET_LENGTH);
